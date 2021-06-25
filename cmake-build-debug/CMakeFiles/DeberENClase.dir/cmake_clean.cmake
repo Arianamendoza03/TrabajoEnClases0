37325b150663b916cbc48ea34f1e73bb1f0c2b8b
ArianaMendonza03 <mendozaariana954@gmail.com>
2021-06-25 15:36:51 +1200

    TrabajoEnClases

---
+++ cmake-build-debug/CMakeFiles/DeberENClase.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DeberENClase.dir/main.cpp.obj"
+  "DeberENClase.exe"
+  "DeberENClase.exe.manifest"
+  "DeberENClase.pdb"
+  "libDeberENClase.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DeberENClase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
